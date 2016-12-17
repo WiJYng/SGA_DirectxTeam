4f099643273c476b2d0c67b8988fa50b4ca50489
WiJYng <wijy1010@gmail.com>
2016-12-18 00:58:49 +0900

    effect테스트중

--- sgaDirectx_TERA/Direct3D_025/cLocationEffect.cpp
+++ sgaDirectx_TERA/Direct3D_025/cLocationEffect.cpp
@@ -26,17 +26,25 @@ void cLocationEffect::Render()
 	DWORD prevLight;
 
 	Device->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
+	Device->SetRenderState(D3DRS_ZWRITEENABLE, false);
 	Device->SetRenderState(D3DRS_ALPHATESTENABLE, TRUE);
+	Device->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_SUBTRACT);
+	
 	Device->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_SRCALPHA);
 	Device->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
-
+	
+	//==============================================================
+	
 	for each(auto v in m_vecMesh)
 	{
 		v->Render();
 	}
 
+	//==============================================================
+
 	Device->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);
 	Device->SetRenderState(D3DRS_ALPHATESTENABLE, FALSE);
+	Device->SetRenderState(D3DRS_ZWRITEENABLE, true);
 }
 
 void cLocationEffect::PlayEffect(EFFECT_NAME _Name, D3DXVECTOR3 pos)
@@ -82,7 +90,7 @@ void cLocationEffect::InitLocationEffect()
 	{
 		D3DXMATRIXA16 mat;
 		D3DXMatrixIdentity(&mat);
-		D3DXMatrixScaling(&mat, 0.1, 0.1, 0.1);
+		//D3DXMatrixScaling(&mat, 0.1, 0.1, 0.1);
 
 		cXMesh_Static* m = RESOURCE_STATICXMESH->GetResource("./Tera/Effect/E_SYLINDER.X", mat);
 		cBaseObject* o = new cBaseObject;
